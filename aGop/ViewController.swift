60f8fefdb04b9af6bcb1cc168e6ceb1c41dac1d9
kailly1231 <student@local111.bcserdika.com>
2017-01-29 19:00:55 +0200

    Initial Commit

---
+++ aGop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aGop
+//
+//  Created by Student on 1/29/17.
+//  Copyright © 2017 Kalina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
